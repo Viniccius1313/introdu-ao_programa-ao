d4ceb9fea30a4a2588e2e8003215075caa976baf
Viniccius1313 <vinnyciushen123@gmail.com>
2025-09-15 08:01:22 -0300

    Update exemplo1.c

--- linguagem C/exemplo1.c
+++ linguagem C/exemplo1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main(
+int main()
+{
+    int a=10, b=3;
+    
+    printf("Soma: %d\n", a+b);
+    printf("Subtraçao: %d\n", a-b);
+    printf("Multiplicaçao %d\n", a*b)
+    printf("Divisao: %d\n", a/b)
+    printf("Modulo(resto da divisao):%d\n", a%b);
+    
+    Float c=10, d=3.0;
+    printf("divisao: %.2f\n", c/d);
 
-printf("Hello World");
-return (0);
-)
+    
+    
+    return 0;
